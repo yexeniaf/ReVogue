737208788154468654e0a8ab9a11a4614f4a696c
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-24 16:31:20 -0500

    addition of validates in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :products, dependent: :destroy
     has_many :reviews, dependent: :destroy
     has_secure_password 
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, length: { minimum: 6 }  
 end
