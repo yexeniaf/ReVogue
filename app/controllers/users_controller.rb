d6ec9a138ac89e493e608b19e4e47dd9c62d174c
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-24 16:44:25 -0500

    updated create on suer controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: {
+        user: @user.attributes.except("password_digest"),
+        token: @token
+    }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
