2ab57fc1f4877c9a540f247cba199d293fff7f7e
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-24 16:29:36 -0500

    routes restructured

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :items
   resources :users
+
+  resources :items do
+    resources :comments
+  end
+  
+  
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
