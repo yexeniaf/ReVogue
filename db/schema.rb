3b5c68efdf58518f48d058726c9eb01736fb4c2d
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-24 16:02:50 -0500

    migrate of comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_205405) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_210014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.boolean "potential_buyer"
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.text "image_url"
@@ -35,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_205405) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
   add_foreign_key "items", "users"
 end
