4a7419d780ca0eebb617d77a9b020350394c404d
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-25 14:04:51 -0500

    login page with boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import {useEffect, useState } from 'react';
 import Home from './components/Home';
 import ItemsContainer from './containers/ItemsContainer';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element= {<Home />} />
         <Route path="/items" element= {<ItemsContainer />} />
-        <Route path="/login" element= {<h1>Login Page</h1>} />
+        <Route path="/login" element= {<Login />} />
         <Route path="/signup" element= {<h1>Sign Up Page</h1>} />
       </Routes>
     </div>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <h1>Login Page</h1>
+    </div>
   )
 }
