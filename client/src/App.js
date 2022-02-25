61cc3da04138b8d47e0867bb4358814ded689a48
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-25 10:36:07 -0500

    setup of routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element= {<h1>Welcome to the homepage</h1>} />
+        <Route path="/items" element= {<h1>Items Page</h1>} />
+        <Route path="/login" element= {<h1>Login Page</h1>} />
+        <Route path="/signup" element= {<h1>Sign Up Page</h1>} />
       </Routes>
     </div>
   );
