47623beca60b4a6fbed5c9a81e8842da6141cd96
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-25 10:31:16 -0500

    typo in index js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element= {<h1>Welcome to the homepage</h1>} />
+      </Routes>
     </div>
   );
 }
