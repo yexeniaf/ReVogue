fe307c4be372b6f4d23f4f4a98dbb353c7767a3e
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-25 15:09:04 -0500

    creating sign up form

--- client/src/App.js
+++ client/src/App.js
@@ -18,13 +18,15 @@ useEffect (() => {
   getUser()
 }, [])
 
+
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element= {<Home />} />
         <Route path="/items" element= {<ItemsContainer />} />
-        <Route path="/login" element= {<Login />} />
-        <Route path="/signup" element= {<Signup />} />
+        <Route path="/login" element= {<Login setCurrentUser={setCurrentUser}/>} />
+        <Route path="/signup" element= {<Signup setCurrentUser={setCurrentUser} />} />
       </Routes>
     </div>
   );
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {useState} from 'react';
+import { signupUser } from '../screens/users';
+import { useNavigate } from 'react-router-dom';
 
-export default function Signup() {
+export default function Signup(props) {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+
   return (
-    <div>
-        <h1>Sign Up Page</h1>
-    </div>
+    <form onSubmit={async (e) => {
+        e.preventDefault()
+        const user = {
+            username, 
+            email, 
+            password
+        }
+        const res = signupUser({user})
+        props.setCurrentUser(res)
+
+        // navigate("/")
+    }}>
+        <input
+            type = "text"
+            value ={username}
+            onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+            type = "text"
+            value ={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+            type = "password"
+            value ={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        <button> Sign Up</button>
+
+    </form>
   )
 }
