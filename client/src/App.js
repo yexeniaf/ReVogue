0debc5d867f210ced131e254b5cb9a8e271f5616
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-25 14:01:06 -0500

    home page boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import {useEffect, useState } from 'react';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element= {<h1>Welcome to the homepage</h1>} />
+        <Route path="/" element= {<Home />} />
         <Route path="/items" element= {<h1>Items Page</h1>} />
         <Route path="/login" element= {<h1>Login Page</h1>} />
         <Route path="/signup" element= {<h1>Sign Up Page</h1>} />
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Welcome to ReVogue</h1>
+    </div>
+  )
+}
