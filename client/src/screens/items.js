3af89fd60e2f565e202603ec1cc14a0c1470583b
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-26 00:52:48 -0500

    fixed errors on items js

--- client/src/screens/items.js
+++ client/src/screens/items.js
@@ -11,16 +11,16 @@ export const getOneItem  = async (item_id) => {
 }
 
 export const createItem  = async (itemData) => {
-    const res = await api.get('/items', {item: itemData})
+    const res = await api.post('/items', {item: itemData})
     return res.data
 }
 
 export const updateItem  = async (item_id, itemData) => {
-    const res = await api.get(`/items/${item_id}`, {item: itemData})
+    const res = await api.put(`/items/${item_id}`, {item: itemData})
     return res.data
 }
 
 export const deleteItem  = async (item_id) => {
-    const res = await api.get(`/items/${item_id}`)
+    const res = await api.delete(`/items/${item_id}`)
     return res.data
 }
