6170a4816036d8db7126f395c297a50c02811c7e
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-27 18:05:45 -0500

    imported comments to item detail, need comments.jsx file

--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -1,7 +1,6 @@
 import {useState, useEffect} from 'react';
-import { useParams, Link } from 'react-router-dom';
-
-
+import {useParams, Link } from 'react-router-dom';
+import { getItemComments, createComment, deleteComment } from '../services/comments';
 
 export default function ItemDetail() {
   return (
