b32096d8a1e078da4931f753b7f08454446cdbef
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-27 21:06:32 -0500

    create item

--- client/src/components/CreateItem.jsx
+++ client/src/components/CreateItem.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { useState } from 'react'
 
-export default function CreateItem() {
+export default function CreateItem(props) {
+
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState(0)
+  const [size, setSize] = useState('')
+  const [condition, setCondiiton] = useState('')
+  const [category, setCategory] = useState('')
+  const [image_url, setImage_url] = useState('')
   return (
-    <div>CreateItem</div>
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      const product = {
+        title,
+        price,
+        size,
+        category,
+        image_url
+      }
+      props.handleCreate(product)
+    }}>
+      <input
+        type='text'
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <input
+        type='number'
+        onChange={(e) => setPrice(e.target.valueAsNumber)}
+        value={price}
+      />
+      <input
+        type='text'
+        onChange={(e) => setSize(e.target.value)}
+        value={size}
+      />
+      <input
+        type='text'
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+      />
+      <input
+        type='text'
+        onChange={(e) => setImage_url(e.target.value)}
+        value={image_url}
+      />
+      <button>Create</button>
+    </form>
   )
 }
