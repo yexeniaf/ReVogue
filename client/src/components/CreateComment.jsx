80a1e38207d23808ebe27591bf91f7b46491bde9
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-03-01 15:35:40 -0500

    linting on create comment

--- client/src/components/CreateComment.jsx
+++ client/src/components/CreateComment.jsx
@@ -1,21 +1,20 @@
 import {useState} from 'react'
 
 export default function CreateComment(props) {
-    console.log(props)
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
 
 
   return (
-    <form className='  bg-neutral-200 flex flex-col p-6 mb-12' 
+    <form className='bg-neutral-200 flex flex-col p-6 mb-12' 
         onSubmit={(e) => {
-        e.preventDefault()
-        const comment = {
-            title,
-            content
-        }
-        props.handleCommentCreate(comment)
-    }}>
+            e.preventDefault()
+            const comment = {
+                title,
+                content
+            }
+            props.handleCommentCreate(comment)
+        }}>
         <label>Title:</label>
         <input
             type= 'text'
@@ -30,9 +29,9 @@ export default function CreateComment(props) {
             onChange={(e) => setContent(e.target.value)}
         />
         <br/>
-        <button className='  className="mt-10 w-full bg-lime-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-        >Submit Comment!</button>
-
+        <button className='  className="mt-10 w-full bg-lime-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-600'>
+            Submit Comment!
+        </button>
     </form>
   )
 }
