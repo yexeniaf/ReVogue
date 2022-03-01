691af08a34c6aea3337da17b0bddb591c2b99089
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-03-01 02:07:24 -0500

    footer fixed in rest of pages

--- client/src/components/CreateComment.jsx
+++ client/src/components/CreateComment.jsx
@@ -7,7 +7,7 @@ export default function CreateComment(props) {
 
 
   return (
-    <form className='border-4 flex flex-col p-6' 
+    <form className='border-4 flex flex-col p-6 mb-12' 
         onSubmit={(e) => {
         e.preventDefault()
         const comment = {
@@ -30,7 +30,7 @@ export default function CreateComment(props) {
             onChange={(e) => setContent(e.target.value)}
         />
         <br/>
-        <button className='className="mt-10 w-full bg-lime-800 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+        <button className='  className="mt-10 w-full bg-lime-800 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
         >Submit Comment!</button>
 
     </form>
--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -41,62 +41,59 @@ export default function ItemDetail(props) {
   return (
     <div className="pt-6">
         <div className=' flex  ml-4 right-0 text- font-sm tracking-tight text-gray-900 sm:text-3xl'>
-        <Link to="/items">Back</Link>
+            <Link to="/items">Back</Link>
         </div>
         {
             item?.id ?
-            <>
-                <div className="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
-                    <div>
-                    <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4 border-4">
-                        <img src={item.image_url} alt={item.image_url} className="w-full h-full object-center object-cover" />
-                    </div>
+                <>
+                    <div className="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+                        <div>
+                            <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4 border-4">
+                                <img src={item.image_url} alt={item.image_url} className="w-full h-full object-center object-cover" />
+                            </div>
+                            <br/>
+                            <div className="mt-4 lg:mt-0 lg:row-span-3 ">
+                                <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8 border-4 ">
+                                    <h2>Product information</h2>
+                                    <br/>
+                                    <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{item.title}</h1>
+                                    <h4 className="text-2xl text-gray-900">${item.price}</h4>
+                                    <br/>
+                                    <div className="mt-4 lg:mt-0 lg:row-span-3">
+                                        <h4>Size: {item.size}</h4>
+                                        <h4 className="text-base text-gray-900"> Condition: {item.condition}</h4>
+                                        <h5 className="text-sm font-medium text-gray-900"> Category: {item.category}</h5>
+                                    </div>
+                                </div>
                 
-                <br/>
-                <div className="mt-4 lg:mt-0 lg:row-span-3 ">
-                <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8 border-4">
-                <h2>Product information</h2>
-                <br/>
-                <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{item.title}</h1>
-                <h4 className="text-2xl text-gray-900">${item.price}</h4>
-                <br/>
-                <div className="mt-4 lg:mt-0 lg:row-span-3">
-                <h4>Size: {item.size}</h4>
-                <h4 className="text-base text-gray-900"> Condition: {item.condition}</h4>
-                <h5 className="text-sm font-medium text-gray-900"> Category: {item.category}</h5>
-                </div>
-                </div>
-                
-                </div>
+                            </div>
 
-                {
-                    props.currentUser?.id === item.user_id ?
-                    <>
-                        <Link to={`/items/${item.id}/edit`}>
-                            <button>Edit Item</button>
-                        </Link>
-                        <button onClick={() => props.handleDelete}> Delete Item</button>
-                    </>
-                    :
-                    null
-                }
-                </div>
-                <Comments
-                    currentUser = {props.currentUser}
-                    comments = {comments}
-                    handleCommentDelete={handleCommentDelete}
-                />
-                <br/>
-                <br/>
+                            {
+                                props.currentUser?.id === item.user_id ?
+                                    <>
+                                        <Link to={`/items/${item.id}/edit`}>
+                                            <button>Edit Item</button>
+                                        </Link>
+                                        <button onClick={() => props.handleDelete}> Delete Item</button>
+                                    </>
+                                :
+                                null
+                            }
+                        </div>
+                        <Comments
+                            currentUser = {props.currentUser}
+                            comments = {comments}
+                            handleCommentDelete={handleCommentDelete}
+                        />
+                        <br/>
+                        <br/>
+                        <br/>
+                        <CreateComment className="sticky bottom-0" handleCommentCreate={ handleCommentCreate}/>
                 
-                <br/>
-                <CreateComment className="sticky bottom-0" handleCommentCreate={ handleCommentCreate}/>
-                
-                </div>
-            </>
+                    </div>
+                </>
             :
-            <h3>No Item Found</h3>
-            
+            <h3>No Item Found</h3>  
         }
     </div>
   )
