7d67d37b343aa793275ff2244896b77249541dc9
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-03-02 01:24:01 -0500

    comment section styling

--- client/src/components/CreateComment.jsx
+++ client/src/components/CreateComment.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 
 export default function CreateComment(props) {
+    
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
 
@@ -28,7 +29,7 @@ export default function CreateComment(props) {
             onChange={(e) => setContent(e.target.value)}
         />
         <br/>
-        <button className='  className="mt-10 w-full bg-lime-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-600'>
+        <button className=' className="mt-10 w-full bg-lime-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-600'>
             Submit Comment!
         </button>
     </form>
