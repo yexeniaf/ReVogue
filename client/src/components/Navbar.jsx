53c0d00ec1fe962ce63cc44fe65906edb6d1cc00
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-03-01 20:07:49 -0500

    nav bar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {Link, Route} from 'react-router-dom'
 import { Disclosure} from '@headlessui/react'
 import {MenuIcon, XIcon } from '@heroicons/react/outline'
+import logo from "../images/logo.png"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -15,7 +16,7 @@ export default function Navbar(props) {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
             <div className="relative flex items-center justify-between h-16 ">
               <div className="absolute inset-y-0 right-0 flex items-center sm:hidden ">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-lime-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
@@ -24,20 +25,27 @@ export default function Navbar(props) {
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start ">
+                <div className="flex-shrink-0 flex items-center">
+                  <img 
+                    src= {logo}
+                    className="block h-10 w-auto"
+                    alt="logo"
+                  />
+                </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    <Link to="/"><h1>ReVogue</h1></Link>
-                    <Link  to='/items'>Items</Link>
+                    <Link className="bg-lime-800 text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/"><h1>Home</h1></Link>
+                    <Link className="bg-lime-800 text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium" to='/items'>Items</Link>
                     {props.currentUser ?
                       <>
-                        {props.currentUser.is_admin && <Link to="/items/create">New Item</Link>}
-                         {props.currentUser && <h3>Welcome, {props.currentUser.username}!</h3>}
-                        <button onClick={props.logout}>Log Out</button>
+                        {props.currentUser.is_admin && <Link className="text-white hover:bg-lime-900  hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/items/create">New Item</Link>}
+                        <button className="text-white hover:bg-lime-900  hover:text-white block px-3 py-2 rounded-md text-base font-medium"  onClick={props.logout}>Log Out</button>
+                        {props.currentUser && <h3 className="bg-lime-800 text-white block px-3 py-2 rounded-md text-base font-medium" >Welcome, {props.currentUser.username}!</h3>}
                       </>
                       :
                       <>
-                        <Link to='/login'>Login</Link>
-                        <Link to='/signup'>Sign Up</Link>
+                        <Link className="bg-lime-800 text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"to='/login'>Login</Link>
+                        <Link className="bg-lime-800 text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium" to='/signup'>Sign Up</Link>
                       </>
                     }
                   </div>
@@ -48,20 +56,21 @@ export default function Navbar(props) {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
                 <div>
-                  <Link to='/' className='text-white'>Home</Link>
+                  <Link  to='/' className="text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</Link>
                 </div>
                 <div>
-                  <Link to='/items' className='text-gray-200'>Items</Link>
+                  <Link to='/items' className="text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Items</Link>
                 </div>
               {props.currentUser ?
                 <>
-                  <h3 className='text-gray-200'>Welcome, {props.currentUser.username}!</h3>
-                  <button className='text-gray-200' onClick={props.logout}>Log Out</button>
+                  {props.currentUser.is_admin && <Link className="text-white hover:bg-lime-900  hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/items/create">New Item</Link>}
+                  <h3 className="text-white  block px-3 py-2 rounded-md text-base font-medium">Welcome, {props.currentUser.username}!</h3>
+                  <button className="text-white block px-3 py-2 rounded-md text-base font-medium" onClick={props.logout}>Log Out</button>
                 </>
                 :
                 <>
-                  <Link to='/login'>Login</Link>
-                  <Link to='/signup'>Sign Up</Link>
+                  <Link className="text-white hover:bg-lime-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium" to='/login'>Login</Link>
+                  <Link className="text-white hover:bg-lime-900  hover:text-white block px-3 py-2 rounded-md text-base font-medium" to='/signup'>Sign Up</Link>
                 </>
               }
             </div>
